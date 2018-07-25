59fe5ec3ece6bf2947c8e8499f40503c61739a6e
lizx <aa234258202@qq.com>
2018-07-25 17:33:34 +0800

    2 bug

--- models/user.py
+++ models/user.py
@@ -70,8 +70,6 @@ class User(object):
                                gcm_device_token, jp_device_token):
         now = int(time.time())
         key = "users_%d_%d"%(appid, uid)
-        s = "123%d"%key
-        print(s)
 
         if device_token:
             obj = {
